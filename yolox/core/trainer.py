6db732def7aaec4e07c942e1ab2722223c82284f
Even <765305261@qq.com>
2021-12-13 14:13:36 +0800

    Add MCMOT module.

--- yolox/core/trainer.py
+++ yolox/core/trainer.py
@@ -196,11 +196,8 @@ class Trainer:
         """
         :return:
         """
-        logger.info(
-            "Training of experiment is done and the best AP is {:.2f}".format(
-                self.best_ap * 100
-            )
-        )
+        logger.info("Training of experiment is done and the best AP is {:.2f}"
+            .format(self.best_ap * 100))
 
     def before_epoch(self):
         """
@@ -231,9 +228,10 @@ class Trainer:
 
         self.save_ckpt(ckpt_name="latest")
 
-        if (self.epoch + 1) % self.exp.eval_interval == 0 and self.exp.eval_interval != 0:
-            all_reduce_norm(self.model)
-            self.evaluate_and_save_model()
+        if self.exp.eval_interval != 0:
+            if (self.epoch + 1) % self.exp.eval_interval == 0:
+                all_reduce_norm(self.model)
+                self.evaluate_and_save_model()
 
     def before_iter(self):
         """
