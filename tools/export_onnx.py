d3481fdc79a04a93595d2f196da65e8e48258f9e
Even <765305261@qq.com>
2022-04-08 10:54:49 +0800

    Add darknet(cfg) backbone.

--- tools/export_onnx.py
+++ tools/export_onnx.py
@@ -1,47 +1,62 @@
-from loguru import logger
+# encoding=utf-8
+
+import argparse
+import os
 
 import torch
+from loguru import logger
 from torch import nn
 
 from yolox.exp import get_exp
 from yolox.models.network_blocks import SiLU
 from yolox.utils import replace_module
 
-import argparse
-import os
-
 
 def make_parser():
     parser = argparse.ArgumentParser("YOLOX onnx deploy")
-    parser.add_argument(
-        "--output-name", type=str, default="bytetrack_s.onnx", help="output name of models"
-    )
-    parser.add_argument(
-        "--input", default="images", type=str, help="input node name of onnx model"
-    )
-    parser.add_argument(
-        "--output", default="output", type=str, help="output node name of onnx model"
-    )
-    parser.add_argument(
-        "-o", "--opset", default=11, type=int, help="onnx opset version"
-    )
-    parser.add_argument("--no-onnxsim", action="store_true", help="use onnxsim or not")
-    parser.add_argument(
-        "-f",
-        "--exp_file",
-        default=None,
-        type=str,
-        help="expriment description file",
-    )
-    parser.add_argument("-expn", "--experiment-name", type=str, default=None)
-    parser.add_argument("-n", "--name", type=str, default=None, help="model name")
-    parser.add_argument("-c", "--ckpt", default=None, type=str, help="ckpt path")
-    parser.add_argument(
-        "opts",
-        help="Modify config options using the command-line",
-        default=None,
-        nargs=argparse.REMAINDER,
-    )
+    parser.add_argument("--output-name",
+                        type=str,
+                        default="bytetrack_s.onnx",
+                        help="output name of models")
+    parser.add_argument("--input",
+                        default="images",
+                        type=str,
+                        help="input node name of onnx model")
+    parser.add_argument("--output",
+                        default="output",
+                        type=str,
+                        help="output node name of onnx model")
+    parser.add_argument("-o",
+                        "--opset",
+                        default=11,
+                        type=int,
+                        help="onnx opset version")
+    parser.add_argument("--no-onnxsim",
+                        action="store_true",
+                        help="use onnxsim or not")
+    parser.add_argument("-f",
+                        "--exp_file",
+                        default=None,
+                        type=str,
+                        help="expriment description file", )
+    parser.add_argument("-expn",
+                        "--experiment-name",
+                        type=str,
+                        default=None)
+    parser.add_argument("-n",
+                        "--name",
+                        type=str,
+                        default=None,
+                        help="model name")
+    parser.add_argument("-c",
+                        "--ckpt",
+                        default=None,
+                        type=str,
+                        help="ckpt path")
+    parser.add_argument("opts",
+                        help="Modify config options using the command-line",
+                        default=None,
+                        nargs=argparse.REMAINDER, )
 
     return parser
 
