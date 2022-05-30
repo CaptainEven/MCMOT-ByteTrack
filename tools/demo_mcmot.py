31433e07eb0371510f5a94459a8fa7b52fe6d722
Even <765305261@qq.com>
2022-05-30 17:15:32 +0800

    Enhance ByteTrack with OC_SORT.

--- tools/demo_mcmot.py
+++ tools/demo_mcmot.py
@@ -504,7 +504,7 @@ def video_tracking(predictor, cap, save_path, opt):
                     else:
                         # online_dict = tracker.update_mcmot_byte(dets, img_size, net_size)
                         # online_dict = tracker.update_byte_nk(dets, img_size, net_size)
-                        online_dict = tracker.update_byte_enhance(dets, img_size, net_size)
+                        online_dict = tracker.update_byte_enhance2(dets, img_size, net_size)
                         # online_dict = tracker.update_oc_enhance2(dets, img_size, net_size)
 
                 elif opt.tracker == "oc":
