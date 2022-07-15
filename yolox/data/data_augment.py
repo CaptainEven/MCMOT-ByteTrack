32728847476df5ba1c3f4421dadcc53f2cfc14c9
Even <765305261@qq.com>
2022-07-15 16:44:16 +0800

    Add new Data Augmentation methods.

--- yolox/data/data_augment.py
+++ yolox/data/data_augment.py
@@ -553,7 +553,6 @@ class TwoCropsTransform:
     """
     Take two random crops of one image as the query and key.
     """
-
     def __init__(self, base_transform):
         self.base_transform = base_transform
 
