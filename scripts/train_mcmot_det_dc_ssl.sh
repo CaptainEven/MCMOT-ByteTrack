c24a99caff710e5f6364fb419431ff03817f4066
Even <765305261@qq.com>
2022-07-27 09:52:06 +0800

    Add new Data Augmentation methods.

--- scripts/train_mcmot_det_dc_ssl.sh
+++ scripts/train_mcmot_det_dc_ssl.sh
@@ -1,7 +1,7 @@
 python3 ./tools/train_det.py --ckpt ./pretrained/v5.46.weights \
                              --exp_file ./exps/example/mot/yolox_det_c5_dark_ssl.py \
                              --cfg ./cfg/yolox_darknet_tiny_bb46.cfg \
-                             --batch-size 10 \
+                             --batch-size 6 \
                              --cutoff 44 \
                              --debug 0 \
                              --n_workers 2 \
--- yolox/models/yolox_dark.py
+++ yolox/models/yolox_dark.py
@@ -176,8 +176,8 @@ class YOLOXDarkSSL(nn.Module):
                 p2_recon = self.head.upsample_fuse_4(p2_recon, p2_maps[0])
                 p2_recon = self.head.upsample_conv(p2_recon)
 
-                reconstruct_loss += self.head.mse_loss(p1_recon, p0)
-                reconstruct_loss += self.head.mse_loss(p2_recon, p0)
+                reconstruct_loss += self.head.mse_loss(p1_recon, p0_patches)
+                reconstruct_loss += self.head.mse_loss(p2_recon, p0_patches11)
 
                 # ## ----- Calculate similarity matrix loss
                 # sm_output = torch.mm(p1_vectors, p2_vectors.T)
